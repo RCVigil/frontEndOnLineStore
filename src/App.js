2a5345cdc6663e8ee6bebef7c760c780926a1ca2
malumahet <malumahet@gmail.com>
2022-07-07 18:28:47 -0300

    Feita q3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Search from './components/Search';
+import Carrinho from './pages/Carrinho';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div>
         <BrowserRouter>
           <Route path="/" component={ Search } />
+          <Route exact path="/carrinho" component={ Carrinho } />
         </BrowserRouter>
       </div>
     );
