ca63d50c1c029fdc7b4d6692d437c0ea3bcd4465
malumahet <malumahet@gmail.com>
2022-07-11 15:52:55 -0300

    Iniciada q13

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -5,6 +5,7 @@ import { getProductsFromCategoryAndQuery } from '../services/api';
 import Loading from '../components/Loading';
 import imgCart from '../images/shopping-cart.png';
 import imgSearch from '../images/search.png';
+import Purchased from '../components/Purchased';
 
 class Search extends Component {
   constructor() {
@@ -122,6 +123,9 @@ class Search extends Component {
               >
                 <img src={ imgCart } alt="cart" width="40px" />
               </Link>
+
+              {/* <Purchased /> */}
+
               <input
                 className="inputSearch"
                 type="text"
